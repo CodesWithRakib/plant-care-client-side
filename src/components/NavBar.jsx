04964500f83139e002a544de7cbf1803f9949541
Md. Rakib Islam <r.s.rakib.business@gmail.com>
2025-05-20 14:21:03 +0600

    navbar added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import { NavLink } from "react-router";
+import logo from "/logo.jpg";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className="flex justify-between items-center bg-gray-800 text-white p-4">
+      <div className="flex gap-2 items-center">
+        <figure>
+          <img src={logo} alt="" className="w-12 h-12 rounded-full" />
+        </figure>
+        <div className="flex flex-col">
+          <h3 className="text-xl ">Green Nest</h3>
+          <p className="text-sm font-thin">Care Your Plants</p>
+        </div>
+      </div>
+      <ul className="flex gap-5 items-center text-lg font-light">
+        <li>
+          {" "}
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/all-plants"}>All Plants</NavLink>
+        </li>
+        <li>
+          {" "}
+          <NavLink to="/my-plants">My Plants</NavLink>
+        </li>
+        <li>
+          {" "}
+          <NavLink to="/add-plant">Add Plant</NavLink>
+        </li>
+      </ul>
+
+      <div className="flex gap-2 items-center text-lg">
+        <NavLink
+          className=" bg-green-600 px-6 py-1 rounded-4xl flex items-center"
+          to="/login"
+        >
+          Login
+        </NavLink>
+        <NavLink
+          className=" bg-green-600 px-6 py-1 rounded-4xl flex items-center"
+          to="/register"
+        >
+          Register
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import { Outlet } from "react-router";
 
 const Root = () => {
-  return <div>Root</div>;
+  return (
+    <div>
+      <NavBar></NavBar>
+      <div>
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Root;
